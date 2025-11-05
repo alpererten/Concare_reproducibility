899427fa10010f27fc7096baeefa3bf82daf6849
Daniel Cho <danielairycareer@gmail.com>
2025-11-04 22:26:43 -0500

    Add a comparison between two metrics: author's and Alper's

---
+++ compare_metrics.sh
@@ -0,0 +1,3 @@
+ # A driver script that compare original author's metrics and our metrics
+ # Remember to switch to environments with the discrepancies
+ python -m unittest Regenerated_Project_Code.test.test_metrics
