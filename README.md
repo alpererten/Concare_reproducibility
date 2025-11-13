5c84dae0135a91ba2a7e0880fd00849711210224
Alper Erten <aerten3@gatech.edu>
2025-11-12 16:06:10 -0800

    cleaned up folder for submission prep

--- README.md
+++
@@ -1,2 +0,0 @@
-# Concare_reproducibility
-Reproduction of the ConCare model (AAAI 2020) on MIMIC-III dataset
--- compare_metrics.sh
+++
@@ -1,3 +0,0 @@
- # A driver script that compare original author's metrics and our metrics
- # Remember to switch to environments with the discrepancies
- python -m unittest Regenerated_Project_Code.test.test_metrics
